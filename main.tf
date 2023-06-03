e6bf6fd170cc93796338e1348a893a2d5ff8a30a
renemaboh <121076586+renemaboh@users.noreply.github.com>
2023-06-03 18:13:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,4 +6,11 @@ resource "aws_instance" "my_vm" {
    Name = var.name_tag,
  }
 }
+resource "aws_s3_bucket" "example" {
+  bucket = "myfirstjenkinsbucket"
 
+  tags = {
+    Name        = "renebucket22"
+    Environment = "Dev"
+  }
+}
